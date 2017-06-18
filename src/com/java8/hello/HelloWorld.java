e0a4bc9402ce7b41f6278346bab22de0b17f1023
Amritesh Kumar <amriteshkumarsingh@gmail.com>
2017-06-18 12:30:17 +0530

    Java8 : Hello : HelloWorld

---
+++ src/com/java8/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java8.hello;
+
+/**
+ * Created by Amritesh Kumar on 18/06/17.
+ *
+ * Hello World! program in Java 8 */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
